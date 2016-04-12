74b0b4aac896e36ddce38db3e95f0a1750bafae5
maximus <maxim.titaruk@ya.ru>
2016-04-12 23:45:26 +0300

    starter now work

--- starter.py
+++ starter.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # __author__ = 'maximus'
+import time
+import sys
+import subprocess
+
+
+def main():
+    start_time = time.time()
+    cli = ['/opt/Zabbix-Network-Weathermap/weathermap.py']
+    cli.extend(sys.argv[1:])
+    subprocess.call(cli)
+    print(round(time.time() - start_time, 3), file=sys.stdout)
+
+if __name__ == '__main__':
+    main()
